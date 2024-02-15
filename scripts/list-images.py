6b1264915b0c4fb1bfab1057fa045f6d8f82d60b
Dominik Rosiek <drosiek@sumologic.com>
2024-02-15 13:20:37 +0100

    feat: add python script to list images
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

---
+++ scripts/list-images.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import argparse
+import re
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--values", help="path to values.yaml", required=True)
+    parser.add_argument("--version", help="helm chart version", default="")
+    return parser.parse_args()
+
+def get_sumo_images(version, values):
+    subprocess.check_output(f'helm repo add sumologic https://sumologic.github.io/sumologic-kubernetes-collection'.split(' '))
+    subprocess.check_output(f'helm repo update'.split(' '))
+    command = f'helm template collection sumologic/sumologic --namespace=sumologic --debug --version={args.version} --values={args.values}'
+    output = subprocess.check_output(command.split(" "))
+
+    matches = re.findall(r'(?:\s*image:\s*|-image=|prometheus-config-reloader=)(.*?)\\n', str(output))
+    if matches == None:
+        sys.exit(-1)
+
+    # use set to remove duplicates
+    return sorted({match.strip('\'"') for match in matches})
+
+if __name__ == '__main__':
+    args = parse_args()
+    images = get_sumo_images(args.version, args.values)
+    
+    for i in images:
+        print(i)
