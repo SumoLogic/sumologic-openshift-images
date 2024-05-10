68152c65b26b3ba4fda24ac8fa059362cdf27ace
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-05-10 13:15:32 +0200

    feat(kube-rbac-proxy): add generic Dockerfile and Makefile (#63)
    * feat(kube-rbac-proxy): add generic Dockerfile and Makefile
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: build automatically kube-rbac-proxy images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: update Makefile
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: add script to automatically build kube-rbac-proxy-images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: update base image
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * Update scripts/build-push-all.sh
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

---
+++ scripts/build-push-all.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+HELM_CHART_VERSION=v3
+SUMO_REGISTRY="public.ecr.aws/sumologic/"
+PUSH=""
+
+IMAGES=$(./scripts/list-images.py \
+    --fetch-base \
+    --values scripts/values.yaml \
+    --version "${HELM_CHART_VERSION}" \
+        | grep kube-rbac-proxy)
+
+for IMAGE in ${IMAGES}; do
+    # Treat everything after `:` as version
+    VERSION="${IMAGE##*:}"
+    # Strip v from version
+    UPSTREAM_VERSION="${VERSION##[v]}"
+    # get name as string between `/` and `:`
+    NAME="${IMAGE%%:*}"
+    NAME="${NAME##*/}"
+    UBI_VERSION="${VERSION}-ubi"
+
+    echo "${SUMO_REGISTRY}${NAME}:${UBI_VERSION}"
+
+    # 
+    if docker pull "${SUMO_REGISTRY}${NAME}:${UBI_VERSION}"; then
+        # as image exist, we can go to the next one
+        continue
+    fi
+
+    make -C ${NAME} build UPSTREAM_VERSION="${UPSTREAM_VERSION}"
+
+    if [[ -n "${PUSH}" ]]; then
+        make -C ${NAME} push UPSTREAM_VERSION="${UPSTREAM_VERSION}"
+    fi
+done
