56f73a338031e710618dec00fc547af96b20f980
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-05-24 14:39:51 +0200

    feat: add common Dockerfile and Makefile for more images (#66)
    * feat: add common Dockerfile and Makefile for more images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * Update opentelemetry-operator/Dockerfile
    
    * Update opentelemetry-operator/Makefile
    
    * feat: fetch for v4
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * fix: fix licence link
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- scripts/build-push-all.sh
+++ scripts/build-push-all.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-HELM_CHART_VERSION=v3
+set -e
+
+HELM_CHART_VERSION=v4
 SUMO_REGISTRY="public.ecr.aws/sumologic/"
 PUSH=""
 
@@ -8,7 +10,7 @@ IMAGES=$(./scripts/list-images.py \
     --fetch-base \
     --values scripts/values.yaml \
     --version "${HELM_CHART_VERSION}" \
-        | grep -E 'kube-rbac-proxy|metrics-server|prometheus-config-reloader|prometheus-operator')
+        | grep -E 'kube-rbac-proxy|metrics-server|prometheus-config-reloader|prometheus-operator|prometheus|opentelemetry-operator|node-exporter|telegraf|telegraf-operator|thanos')
 
 for IMAGE in ${IMAGES}; do
     # Treat everything after `:` as version
