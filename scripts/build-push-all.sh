1121d071c2c8a347a691a96fb2c477eef49fe7e3
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-05-28 09:40:08 +0200

    feat: add remaining images; disable falco; disable remoteWriteProxy (#71)
    * feat: add Makefile and Dockerfile for busybox
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: check all images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: build all images on pr
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: do not build Sumo Logic owned images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: check image only if required
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: do not build tailing sidecar images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: add kube-state-metrics image
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: disable remoteWriteProxy
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- scripts/build-push-all.sh
+++ scripts/build-push-all.sh
@@ -5,12 +5,13 @@ set -e
 HELM_CHART_VERSION=v4
 SUMO_REGISTRY="public.ecr.aws/sumologic/"
 PUSH=""
+CHECK="${CHECK:-true}"
 
 IMAGES=$(./scripts/list-images.py \
     --fetch-base \
     --values scripts/values.yaml \
     --version "${HELM_CHART_VERSION}" \
-        | grep -E 'kube-rbac-proxy|metrics-server|prometheus-config-reloader|prometheus-operator|prometheus|opentelemetry-operator|node-exporter|telegraf|telegraf-operator|thanos|autoinstrumentation|fluent-bit')
+    | grep -vE '\/(sumologic-otel-collector|kubernetes-setup|kubernetes-tools-kubectl|tailing-sidecar-operator|tailing-sidecar):')
 
 for IMAGE in ${IMAGES}; do
     # Treat everything after `:` as version
@@ -25,8 +26,10 @@ for IMAGE in ${IMAGES}; do
 
     echo ${IMAGE_NAME}
     if docker pull ${IMAGE_NAME}; then
+        if [[ "${CHECK}" == "true" ]]; then
+            make -C ${NAME} check IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
+        fi
         # as image exist, we can go to the next one
-        make -C ${NAME} check IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
         continue
     fi
 
@@ -36,5 +39,7 @@ for IMAGE in ${IMAGES}; do
         make -C ${NAME} push IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
     fi
 
-    make -C ${NAME} check IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
+    if [[ "${CHECK}" == "true" ]]; then
+        make -C ${NAME} check IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
+    fi
 done
