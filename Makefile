fd8f393f66588fd36be7ec1ff70ea1ad41b5ee28
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-05-29 12:29:02 +0200

    feat: add scripts/build-push.sh (#76)
    * feat: add scripts/build-push.sh
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: add workflow to certify image manually
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: minor updates
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: test workflow
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: fix ECR credentials
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: install preflight
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: fix preflight installation; remove test workflow
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+OPENSOURCE_ECR_URL = public.ecr.aws/sumologic
+
 list-images-v4:
 	./scripts/list-images.py \
     	--fetch-base \
@@ -14,7 +16,15 @@ build-all:
 	CHECK=false ./scripts/build-push-all.sh
 
 check:
-	PUSH=true CERTIFY=false CHECK=true ./scripts/build-push-all.sh
+	PUSH=true CHECK=true CERTIFY=false ./scripts/build-push-all.sh
 
 certify:
-	PUSH=true CERTIFY=true CHECK=false ./scripts/build-push-all.sh
+	PUSH=true CHECK=true CERTIFY=true ./scripts/build-push-all.sh
+
+_login:
+	aws ecr-public get-login-password --region us-east-1 \
+	| docker login --username AWS --password-stdin $(ECR_URL)
+
+login:
+	$(MAKE) _login \
+		ECR_URL="$(OPENSOURCE_ECR_URL)"
--- scripts/build-push-all.sh
+++ scripts/build-push-all.sh
@@ -8,35 +8,11 @@ CHECK="${CHECK:-true}"
 CERTIFY="${CERTIFY:-false}"
 PUSH="${PUSH:-false}"
 
-## Sumo Logic Helm Operator project id
-## rel: https://connect.redhat.com/manage/products/6075d88c2b962feb86bea730/overview
-readonly OPERATOR_PROJECT_ID=6075d88c2b962feb86bea730
 
 if [[ -z "${PYAXIS_API_TOKEN}" && "${CHECK}" == "true" ]]; then
     echo "PYAXIS_API_TOKEN is required to perform check"
     exit -1
 fi
-
-## Perform image check
-function check(){
-    echo "Checking image, image: ${IMAGE_NAME}"
-    make -C ${NAME} check IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
-}
-
-## Perform image submit for certification
-function submit(){
-    echo "Submitting image for cerification, image: ${IMAGE_NAME}"
-    ## Fetch container project id based on directory(image) name
-    CONTAINER_PROJECT_ID="$(curl -sH "X-API-KEY: ${PYAXIS_API_TOKEN}" "https://catalog.redhat.com/api/containers/v1/product-listings/id/${OPERATOR_PROJECT_ID}/projects/certification" | jq ".data[] | select(.name == \"${NAME}\")._id" --raw-output)"
-    ## Fetch key for image registry
-    CONTAINER_REGISTRY_KEY="$(curl -sH "X-API-KEY: ${PYAXIS_API_TOKEN}" "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${CONTAINER_PROJECT_ID}/secrets" | jq ".registry_credentials.password" --raw-output)"
-
-    CONTAINER_PROJECT_ID=${CONTAINER_PROJECT_ID} \
-    CONTAINER_REGISTRY_KEY=${CONTAINER_REGISTRY_KEY} \
-    SUMOLOGIC_IMAGE=${IMAGE_NAME} \
-    ./scripts/submit_image.sh
-}
-
 
 IMAGES=$(./scripts/list-images.py \
     --fetch-base \
@@ -61,31 +37,11 @@ for IMAGE in ${IMAGES}; do
     IMAGE_NAME="${SUMO_REGISTRY}${NAME}:${UBI_VERSION}${DEV_SUFFIX}"
     echo "Image: ${IMAGE_NAME}"
 
-    if docker pull ${IMAGE_NAME}; then
-        if [[ "${CHECK}" == "true" ]]; then
-            check
-        fi
-
-        if [[ ${DEV_SUFFIX} != "-dev" ]]; then
-            # as non-dev image exists, we can go to the next one
-            # we may want push dev images once again, e.g. with fixes
-            echo "Image ${IMAGE_NAME} exists, there is no need to push it once again, continue with next image."
-            continue
-        fi
-    fi
-
-    make -C ${NAME} build IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
-
-    if [[ "${PUSH}" == "true" ]]; then
-        echo "Pushing image, image: ${IMAGE_NAME}"
-        make -C ${NAME} push IMAGE_NAME=${IMAGE_NAME} UPSTREAM_VERSION="${UPSTREAM_VERSION}"
-    fi
-
-    if [[ "${CHECK}" == "true" ]]; then
-        check
-    fi
-
-    if [[ "${CERTIFY}" == "true" ]]; then
-        submit
-    fi
+   PYAXIS_API_TOKEN="${PYAXIS_API_TOKEN=}" \
+   NAME="${NAME}" \
+   VERSION="${VERSION}" \
+   CHECK="${CHECK}" \
+   PUSH="${PUSH}" \
+   CERTIFY="${CERTIFY}" \
+   ./scripts/build-push.sh
 done
