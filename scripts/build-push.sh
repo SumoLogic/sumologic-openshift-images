ebe290063c90063b97bf502dbfe02a264e935f9f
Dominik Rosiek <drosiek@sumologic.com>
2024-06-03 11:27:23 +0200

    feat: create project if not exist
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,6 +4,9 @@ set -e
 
 # consts
 readonly SUMO_REGISTRY="public.ecr.aws/sumologic/"
+## Sumo Logic Helm Operator project id
+## rel: https://connect.redhat.com/manage/products/6075d88c2b962feb86bea730/overview
+readonly OPERATOR_PROJECT_ID=6075d88c2b962feb86bea730
 
 function usage() {
     echo "usage: NAME=image name VERSION= PYAXIS_API_TOKEN= ./scripts/build-push.sh
@@ -34,7 +37,28 @@ function submit(){
 
     if [[ -z ${CONTAINER_PROJECT_ID} ]]; then
         echo "Missing project for ${IMAGE_NAME}"
-        exit 1
+
+        ## Create new project
+        readonly IMAGE_DESCRIPTION="$(docker inspect "${IMAGE_NAME}" | jq '.[0].Config.Labels.description' --raw-output)"
+        readonly IMAGE_SUMMARY="$(docker inspect "${IMAGE_NAME}" | jq '.[0].Config.Labels.summary' --raw-output)"
+        readonly APPLICATION_NAME="$(docker inspect "${IMAGE_NAME}" | jq '.[0].Config.Labels.name' --raw-output)"
+        export IMAGE_DESCRIPTION
+        export IMAGE_SUMMARY
+        export APPLICATION_NAME
+        DATA="$(envsubst < scripts/new_project.json)"
+        RESULT="$(curl -sH "X-API-KEY: ${PYAXIS_API_TOKEN}" -H 'Content-Type: application/json' -X POST https://catalog.redhat.com/api/containers/v1/projects/certification --data "${DATA}")"
+        
+        ## Add new project to product
+        CONTAINER_PROJECT_ID="$(echo ${RESULT} | jq '._id' --raw-output)"
+
+        if [[ -z ${CONTAINER_PROJECT_ID} ||"${CONTAINER_PROJECT_ID}" == "null" ]]; then
+            echo "Error during create project for ${IMAGE_NAME}"
+            echo $RESULT 
+            exit 1
+        fi
+
+        DATA=$(curl -sH "X-API-KEY: ${PYAXIS_API_TOKEN}" https://catalog.redhat.com/api/containers/v1/product-listings/id/${OPERATOR_PROJECT_ID} | jq ".cert_projects |= . + [\"${CONTAINER_PROJECT_ID}\"] | {cert_projects}")
+        RESULT="$(curl -sH "X-API-KEY: ${PYAXIS_API_TOKEN}" -H 'Content-Type: application/json' -X PATCH https://catalog.redhat.com/api/containers/v1/product-listings/id/${OPERATOR_PROJECT_ID} --data "${DATA}")"
     fi
 
     ## Fetch key for image registry
@@ -65,10 +89,6 @@ readonly ACTION_CHECK="check"
 readonly ACTION_CERTIFY="certify"
 readonly ACTION="${ACTION:-${ACTION_BUILD}}"
 DEV_SUFFIX=""
-
-## Sumo Logic Helm Operator project id
-## rel: https://connect.redhat.com/manage/products/6075d88c2b962feb86bea730/overview
-readonly OPERATOR_PROJECT_ID=6075d88c2b962feb86bea730
 
 if ! [[ "$ACTION" =~ ${ACTION_BUILD}|${ACTION_PUSH}|${ACTION_CHECK}|${ACTION_CERTIFY} ]]; then
     echo "ACTION should be '${ACTION_BUILD}', '${ACTION_PUSH}', '${ACTION_CHECK}' or ${ACTION_CERTIFY}"
