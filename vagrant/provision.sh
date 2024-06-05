aaef4c2d67dd3bd2648beecaff7f1c0852eb9835
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-06-05 14:42:38 +0200

    feat: add arm support to vagrant (#91)
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-set -ex
+set -euo pipefail
+
+LSB_RELEASE="$(lsb_release -cs)"
+ARCH="$(dpkg --print-architecture)"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -9,18 +12,17 @@ apt-get --yes upgrade make awscli
 # Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   "deb [arch=${ARCH}] https://download.docker.com/linux/ubuntu \
+   ${LSB_RELEASE} \
    stable"
-apt-get install --yes docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -aG docker vagrant
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-PREFILIGHT_VERSION="1.9.6"
-wget "https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/${PREFILIGHT_VERSION}/preflight-linux-amd64"
+wget "https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/latest/download/preflight-linux-amd64"
 mv preflight-linux-amd64 preflight
 chmod +x preflight
 mv preflight /usr/local/bin/preflight
